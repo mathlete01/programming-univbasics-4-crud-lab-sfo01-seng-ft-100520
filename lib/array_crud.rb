116b59f204574b29499b2ab7307aa5a8e480dd1c
Matthew Sallin <msallin@gmail.com>
2020-09-22 02:50:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,8 +31,7 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  foo = array.length()
-  array[foo]
+  array[array.length-1]
 end
 
 def update_element_from_index(array, index_number, element)
